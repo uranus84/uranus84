363f79503347c358c6b8ccb70e6a8ca303a96511
aploftus <aploftus@users.noreply.github.com>
2017-11-16 14:28:36 -0800

    Update eslint settings for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,27 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": ["jest"],
+    "extends": [
+      "airbnb",
+      "plugin:react/recommended"
+    ],
+    "plugins": [
+      "jest",
+      "react"
+    ],
+    "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
     "env": {
+      "es6": true,
+      "browser": true,
+      "node": true,
+      "mocha": true,
       "jest/globals": true
+    }, 
+    "rules": {
+      "import/extensions": "off"
     }
 };

64bcf900fd7c2b12ac3bd15b0ca998b48bd1b23c
andy <chaochaoqi@gmail.com>
2017-11-15 17:23:05 -0800

    Add heroku setup files

---
+++ server/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+ 
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});

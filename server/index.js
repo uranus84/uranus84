3c9809dd316f70bf34ce6d5a517cf413c4091a8b
aploftus <aploftus@users.noreply.github.com>
2017-11-15 19:42:41 -0800

    (fix) Change server settings for ENV variable, remove html-webpack-plugin

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + './../'));
 
-app.get('/', (req, res) => res.send('Server Message!'))
+app.get('/', (req, res) => res.send('Server Message!'));
 
 app.get('/test', (req, res) => {
   console.log('test get gotten');
   res.send('Get request success!');
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+let port = process.env.PORT;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

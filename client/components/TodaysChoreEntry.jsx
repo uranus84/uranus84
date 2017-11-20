29f5549da65df9984775c09cc816137519899bd1
aploftus <aploftus@users.noreply.github.com>
2017-11-20 14:58:35 -0800

    (test) Write test skeleton for Task View components

---
+++ client/components/TodaysChoreEntry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodaysChoreEntry(props) {
+  return (<div chores={props.chores}>Todays Chore Entry component</div>);
+}
+
+export default TodaysChoreEntry;
---
+++ client/components/TodaysChores.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodaysChores(props) {
+  return (<div chores={props.chores}>Todays Chores component</div>);
+}
+
+export default TodaysChores;

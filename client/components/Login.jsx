77ef705c268e7a058f4806545f6504144e2585d2
andy <chaochaoqi@gmail.com>
2017-11-20 18:29:09 -0800

    trying to simulate tests for onclick

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import $ from 'jquery';
+// import Axios for all client files making requests
+import axios from 'axios';
 
-class Login extends React.component {
-  constructor() {
-		super();
-		this.state = {
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('inside constructor');
+    this.state = {
 			username: '',
 			password: ''
-		}
-	}
+    }
+  }
+
+  submit() {
+    this.setState({username: this.refs.username.value, password: this.refs.password.value});
+  }
 
 	render (){
 		return (
-			<div id='login'>
-				<input type='text' id='username'></input>
-				<input type='text' id='password'></input>
-				<button type='button' /*onClick={() => {some function}}*/>
-					Login
-				</button>
-				<button type='button' /*onClick={() => {some other function}}}*/>
+			<div>
+				<form action="javascript:void(0)" onSubmit={() => {this.submit()}}>
+					<div>
+						<input ref='username' type='text' id='username'></input>
+					</div>
+					<div>
+						<input ref='password' type='text' id='password'></input>
+					</div>
+					<button type='button' class='login'>
+						Login
+					</button>
+				</form>
+				<button type='button' class='signup' /*onClick={() => {some other function}}}*/>
 					Sign-up
 				</button>
 			</div>

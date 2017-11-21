0f2c6251d0ea518f428076fd5d0195dafd9805ed
Tejaswi <tejaswi.reddy6@gmail.com>
2017-11-20 22:55:25 -0800

    resolved conflicts again

--- database/spec/database-spec.js
+++ database/spec/database-spec.js
@@ -6,8 +6,8 @@ const expect = require('chai').expect;
 const request = require('request');
 
 
-
 describe('Persistent choreApp Server', () => {
+
   let dbConnection;
   beforeEach((done) => {
     dbConnection = mysql.createConnection({
@@ -22,4 +22,25 @@ describe('Persistent choreApp Server', () => {
     dbConnection.query(`truncate ${tablename} ${done}`);
   });
   afterEach(() => dbConnection.end());
+
+
+  it('should insert the chores to the DB', (done) => {
+  // post the new chore into the DB
+    request({
+      method: 'POST',
+      uri: 'http://localhost:3000/chores',
+      json: {},
+    }, () => {
+      // Now if we look into the database,we should find the new chore
+      const queryString = 'SELECT * FROM chores';
+      const queryArgs = [];
+      dbConnection.query(queryString, queryArgs, (err, results) => {
+        // Should have one result:
+        expect(results.length).to.equal(1);
+        // TODO: If you don't have a column named text, change this test.
+        expect(results[0].chore_name).to.equal('');
+        done();
+      });
+    });
+  });
 });

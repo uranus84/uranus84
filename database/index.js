2bb556f60b4669b16437801992715e2790a8ce78
Tejaswi <tejaswi.reddy6@gmail.com>
2017-11-20 22:55:25 -0800

    resolved conflicts again

--- database/index.js
+++ database/index.js
@@ -1,16 +1,12 @@
 const mysql = require('mysql');
 
-
 // Create a db connection & export it from this file
-
 // need to connect to user 'root', no password, and to db 'choreApp'
 let dbConnection = mysql.createConnection({
   user: 'root',
-  password: '',
   database: 'choreApp'
 });
 
 dbConnection.connect();
 
 module.exports = dbConnection;
-

76519fe496d54843b70446d329f16a0ae5d8877b
aploftus <aploftus@users.noreply.github.com>
2017-11-20 16:44:23 -0800

    (feat) Render stateless task view components

--- client/components/FutureChores.jsx
+++ client/components/FutureChores.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import FutureChoreEntry from './FutureChoreEntry.jsx';
 
 function FutureChores(props) {
-  return (<div chores={props.chores}>Future Chores component</div>);
+  return (
+    <div className="container future-chores-list">
+      <div className="row">
+        <h3>Tomorrow and Beyond</h3>
+      </div>
+      {props.chores.map((chore, index) => <FutureChoreEntry chore={chore} key={index} />)}
+    </div>
+  );
 }
 
 export default FutureChores;
--- client/components/TodaysChores.jsx
+++ client/components/TodaysChores.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import TodaysChoreEntry from './TodaysChoreEntry.jsx';
 
 function TodaysChores(props) {
-  return (<div chores={props.chores}>Todays Chores component</div>);
+  return (
+    <div className="container todays-chores-list">
+      <div className="row">
+        <h3>Today</h3>
+      </div>
+      {props.chores.map((chore, index) => <TodaysChoreEntry chore={chore} key={index} />)}
+    </div>
+  );
 }
 
 export default TodaysChores;

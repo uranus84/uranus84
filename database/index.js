1cba325ff246646d5a85168c50231f7173743f42
Kaiser Wu <kaiserwu@Kaisers-MacBook-Pro.local>
2017-11-21 16:52:34 -0800

    Create test file

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 
 // Create a db connection & export it from this file
+
 // need to connect to user 'root', no password, and to db 'choreApp'
 let dbConnection = mysql.createConnection({
   user: 'root',
---
+++ test/server-spec/server-auth-spec.js
@@ -0,0 +1,5 @@
+let mysql = require('mysql');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();

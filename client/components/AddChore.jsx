700a23d0fb6471019c803d5858e6a957f48a8bff
aploftus <aploftus@users.noreply.github.com>
2017-11-22 11:35:01 -0800

    (feat) Render Add Chore component with datepicker
    Add id to dummy data

--- client/components/AddChore.jsx
+++ client/components/AddChore.jsx
@@ -1,25 +1,62 @@
 import React from 'react';
+import Datetime from 'react-datetime';
+import axios from 'axios';
+import moment from 'moment';
 
-function AddChore() {
-  // should have a "custom" frequency picker as well
-  return (
-    <div className="row add-chore">
-      <form id="add-chore-form">
-        <input className="chore-name" type="text" />
-        <input id="datepicker" type="text" />
-        <input id="timepicker" type="text" />
-        <select name="frequency">
-          <option value="Daily">Daily</option>
-          <option value="Daily">Weekly</option>
-          <option value="Daily">Bi-Weekly</option>
-          <option value="Daily">Monthly</option>
-          <option value="Daily">Semi-Annually</option>
-          <option value="Daily">Annually</option>
-        </select>
-        <button type="submit">Add Chore</button>
-      </form>
-    </div>
-  );
+class AddChore extends React.component {
+  // future implementation should have a "custom" frequency picker as well
+  constructor(props) {
+    super(props);
+    this.state = {
+      chore_name: '',
+      next_date: '',
+      frequency: '',
+    };
+  }
+
+  handleDateChange(event) {
+    this.setState({ next_date: moment(event._d).format('YYYY-MM-DD hh:mm:ss') });
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+  handleSubmit() {
+    axios.post('/chores', {
+      chore_name: this.state.chore_name,
+      next_date: this.state.next_date,
+      frequency: this.state.frequency,
+    })
+      .then((response) => {
+        console.log('posted a chore to server!');
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="row add-chore">
+        <form id="add-chore-form" onSubmit={this.handleSubmit}>
+          Chore:
+          <input id="chore_name" type="text" onChange={e => this.handleChange(e)} />
+          Date:
+          <Datetime id="next_date" onChange={e => this.handleDateChange(e)} />
+          Frequency:
+          <select id="frequency" onChange={e => this.handleChange(e)}>
+            <option value="daily">Daily</option>
+            <option value="weekly">Weekly</option>
+            <option value="bi-weekly">Bi-Weekly</option>
+            <option value="monthly">Monthly</option>
+          </select>
+          <button type="submit">Add Chore</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddChore;

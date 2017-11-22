29e1eef163a3438d890508d00f719259f69a98ca
Kaiser Wu <kaiserwu@Kaisers-MacBook-Pro.local>
2017-11-21 17:23:41 -0800

    Create basic skeleton of tests, pending actual content

--- test/server-spec/server-auth-spec.js
+++ test/server-spec/server-auth-spec.js
@@ -3,3 +3,27 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+
+describe('Server Auth Routes', () => {
+  describe('POST to signup', () => {
+    it('Redirects to login on success', () => {
+
+    });
+    it('Redirects back to signup on failure', () => {
+
+    });
+  });
+  describe('POST to login', () => {
+    it('Redirects to home page on success', () => {
+
+    });
+    it('Redirects back to login on failure', () => {
+
+    });
+  });
+  describe('GET to logout', () => {
+    it('Removes user and session info from cookies', () => {
+
+    });
+  });
+});

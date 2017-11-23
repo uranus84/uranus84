78459df1d94a8eb579f5c8eea62306c4ff6dc1e8
Tejaswi <tejaswi.reddy6@gmail.com>
2017-11-22 19:12:48 -0800

    resolved the merge conflicts after rebasing

--- database/index.js
+++ database/index.js
@@ -1,20 +1,18 @@
 const mysql = require('mysql');
 
-
 // Create a db connection & export it from this file
 
 // connection set up to route to ClearDB database on heroku
+// need to connect to user 'root', no password, and to db 'choreApp'
 let dbConnection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
   user: 'b912fe68a70b8b',
   password: '35dc2118',
   database: 'heroku_49fb8337b7fd0ce'
   user: 'root',
-  password: '',
   database: 'choreApp'
 });
 
 dbConnection.connect();
 
 module.exports = dbConnection;
-

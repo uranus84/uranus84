bf7f6441ef9a67cf5b63d913a87cee854bb2e3d2
andy <chaochaoqi@gmail.com>
2017-11-24 19:14:54 -0800

    Updating to make local testing easier

--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 // Create a db connection & export it from this file
 
 // connection set up to route to ClearDB database on heroku
+<<<<<<< 083c34f8ef51298ac222f272e539b8ff1d0bec79
 // need to connect to user 'root', no password, and to db 'choreApp'
 const dbConnection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
@@ -13,6 +14,21 @@ const dbConnection = mysql.createConnection({
   // user: 'root',
   // database: 'choreApp',
   stringifyObjects: true,
+=======
+
+/*******************DATABASE FOR HEROKU*******************/
+// let dbConnection = mysql.createConnection({
+//   host: 'us-cdbr-iron-east-05.cleardb.net',
+//   user: 'b912fe68a70b8b',
+//   password: '35dc2118',
+//   database: 'heroku_49fb8337b7fxd0ce'
+// });
+
+/******************DATABASE FOR LOCAL*********************/
+let dbConnection = mysql.createConnection({
+  user: 'root',
+  database: 'choreApp'
+>>>>>>> Updating to make local testing easier
 });
 
 dbConnection.connect();

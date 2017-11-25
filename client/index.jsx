d33940c3d98f95644044625de7e07ccc99fd1336
aploftus <aploftus@users.noreply.github.com>
2017-11-25 12:07:40 -0800

    (bug) Fix linting errors

--- client/index.jsx
+++ client/index.jsx
@@ -4,32 +4,30 @@ import App from './components/App.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 
+import './style.css';
+
 class Index extends React.Component {
   constructor(props) {
     super(props);
-		this.state = {
-			view: 'login'
-		}
-
+    this.state = {
+      view: 'login',
+    };
     this.changeView = this.changeView.bind(this);
-	}
-
+  }
   changeView(option) {
     this.setState({
-      view: option
+      view: option,
     });
   }
 
   renderView() {
-    const {view} = this.state;
-
-    if (view === 'login') {
-      return <Login handleLogin={this.changeView}/>
-    } else if (view === 'signup') {
-      return <Signup handleSignup={this.changeView}/>
-    } else if (view === 'home'){
-      return <App handleLogout={this.changeView}/>
+    const { view } = this.state;
+    if (view === 'signup') {
+      return <Signup handleSignup={this.changeView} />;
+    } else if (view === 'home') {
+      return <App handleLogout={this.changeView} />;
     }
+    return <Login handleLogin={this.changeView} />;
   }
 
   render() {
@@ -37,7 +35,7 @@ class Index extends React.Component {
       <div className="main">
         {this.renderView()}
       </div>
-    )
+    );
   }
 }
 

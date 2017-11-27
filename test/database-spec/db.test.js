b773ea56143c098ff76337c91c68d51043a4cd4b
aploftus <aploftus@users.noreply.github.com>
2017-11-27 10:37:04 -0800

    (test) Implement dbConnection for db tests

--- test/database-spec/db.test.js
+++ test/database-spec/db.test.js
@@ -5,41 +5,63 @@ const mysql = require('mysql');
 const expect = require('chai').expect;
 const request = require('request');
 
+// require the modelDB in order to run those functions
 
-describe('Persistent choreApp Server', () => {
-
+describe('chore app', () => {
   let dbConnection;
+
   beforeEach((done) => {
     dbConnection = mysql.createConnection({
       user: 'root',
-      password: '',
       database: 'choreApp',
     });
     dbConnection.connect();
-    const tablename = 'chores';
-    /* Empty the db table before each test so that multiple tests
-      * (or repeated runs of the tests) won't screw each other up: */
-    dbConnection.query(`truncate ${tablename} ${done}`);
-  });
-  afterEach(() => dbConnection.end());
 
-  it('should insert the chores to the DB', (done) => {
-  // post the new chore into the DB
-    request({
-      method: 'POST',
-      uri: 'http://localhost:3000/chores',
-      json: {},
-    }, () => {
-      // Now if we look into the database,we should find the new chore
-      const queryString = 'SELECT * FROM chores';
-      const queryArgs = [];
-      dbConnection.query(queryString, queryArgs, (err, results) => {
-        // Should have one result:
-        expect(results.length).to.equal(1);
-        // TODO: If you don't have a column named text, change this test.
-        expect(results[0].chore_name).to.equal('');
-        done();
-      });
+    // Empty the db table before each test so that multiple tests
+    // * (or repeated runs of the tests) won't screw each other up: 
+    dbConnection.query('TRUNCATE users', () => {
+      dbConnection.query('TRUNCATE chores', done);
     });
   });
+
+  afterEach(() => {
+    dbConnection.end();
+  });
+
+  it('should insert users to the DB', (done) => {
+    expect(true).to.equal(true);
+    done();
+  });
+
+  // it('should insert the chores to the DB', (done) => {
+  // // post the new chore into the DB
+  //   request({
+  //     method: 'POST',
+  //     uri: 'http://localhost:3000/chores',
+  //     json: {},
+  //   }, () => {
+  //     // Now if we look into the database,we should find the new chore
+  //     const queryString = 'SELECT * FROM chores';
+  //     const queryArgs = [];
+  //     dbPool.query(queryString, queryArgs, (err, results) => {
+  //       // Should have one result:
+  //       expect(results.length).to.equal(1);
+  //       // TODO: If you don't have a column named text, change this test.
+  //       expect(results[0].chore_name).to.equal('');
+  //       done();
+  //     });
+  //   });
+  // });
+
+  // it('should delete chores from the DB', (done) => {
+
+  // });
+
+  // it('should update chores upon completion', (done) => {
+
+  // });
+
+  // it('should edit chores in the DB', (done) => {
+
+  // });
 });

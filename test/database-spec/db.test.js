41727fc3022f0550bbd6e8777dc26321991089de
aploftus <aploftus@users.noreply.github.com>
2017-11-27 11:51:23 -0800

    (test) Insert into users and chores successfully

--- test/database-spec/db.test.js
+++ test/database-spec/db.test.js
@@ -5,9 +5,11 @@ const mysql = require('mysql');
 const expect = require('chai').expect;
 const request = require('request');
 
+const modelDB = require('../../database/model.js');
+
 // require the modelDB in order to run those functions
 
-describe('chore app', () => {
+describe('TidyUp', () => {
   let dbConnection;
 
   beforeEach((done) => {
@@ -17,10 +19,12 @@ describe('chore app', () => {
     });
     dbConnection.connect();
 
-    // Empty the db table before each test so that multiple tests
-    // * (or repeated runs of the tests) won't screw each other up: 
-    dbConnection.query('TRUNCATE users', () => {
-      dbConnection.query('TRUNCATE chores', done);
+    // Empy the tables. Since users is a foreign key,
+    // delete all items from table and reset auto-increment.
+    dbConnection.query('TRUNCATE chores', () => {
+      dbConnection.query('DELETE FROM users', () => {
+        dbConnection.query('ALTER TABLE users AUTO_INCREMENT = 1', done);
+      });
     });
   });
 
@@ -29,8 +33,30 @@ describe('chore app', () => {
   });
 
   it('should insert users to the DB', (done) => {
-    expect(true).to.equal(true);
-    done();
+    const userQ = 'INSERT INTO users (user_name, password) VALUES ("testUser1", "testPassword1")';
+    dbConnection.query(userQ, () => {
+      dbConnection.query('SELECT * FROM users WHERE user_name = "testUser1"', (err, results) => {
+        expect(results.length).to.equal(1);
+        done();
+      });
+    });
+  });
+
+  it('should insert chores to the DB', (done) => {
+    // const userQ = 'INSERT INTO users (user_name, password) VALUES ("testUser1", "testPassword1")';
+    const choreCols = '(chore_name, next_date, frequency, user_id)';
+    const choreVals = '("test chore", "2017-11-27", "daily", 1)';
+    const choreQ = `INSERT INTO chores ${choreCols} VALUES ${choreVals}`;
+
+    // insert user
+    dbConnection.query('INSERT INTO users (user_name, password) VALUES ("testUser1", "testPassword1")', () => {
+      dbConnection.query(choreQ, () => {
+        dbConnection.query('SELECT * FROM chores WHERE user_id = 1', (err, results) => {
+          expect(results.length).to.equal(1);
+          done();
+        });
+      });
+    });
   });
 
   // it('should insert the chores to the DB', (done) => {
